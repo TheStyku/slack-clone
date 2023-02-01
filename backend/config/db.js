7c566d071edd1c13e910af6857a578be0d6d1962
TheStyku <93047909+TheStyku@users.noreply.github.com>
2023-02-01 18:49:31 +0100

    security fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://thor:mjolnir@slackclone.8eedajj.mongodb.net/test")
+    const conn = await mongoose.connect(process.env.MONGO_URI)
 
     console.log(`MongoDB Connected: ${conn.connection.host}`)
   } catch (error) {
