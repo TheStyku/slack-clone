418d9a5ec8c5e14b5ccc66dece931ce1c8c5b8dd
TheStyku <93047909+TheStyku@users.noreply.github.com>
2023-07-02 17:29:37 +0200

    adding dynamic rooms

--- frontend/src/components/RoomList.jsx
+++ frontend/src/components/RoomList.jsx
@@ -9,52 +9,53 @@ import TagIcon from "@mui/icons-material/Tag";
 import ExpandLess from "@mui/icons-material/ExpandLess";
 import ExpandMore from "@mui/icons-material/ExpandMore";
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 import UserContext from "../context/user/UserContext";
-import { useState,useContext } from "react";
+import { useState, useContext } from "react";
 import axios from "axios";
+import { ListItem } from "@mui/material";
 
-function RoomList({socket}) {
+function RoomList({ socket }) {
   const [open, setOpen] = useState(true);
   const [open1, setOpen1] = useState(true);
   const [open2, setOpen2] = useState(false);
-  const [room, setRoom] = useState('')
-  const {dispatch,token} = useContext(UserContext);
+  const [room, setRoom] = useState("");
+  const { dispatch, token } = useContext(UserContext);
+  const [chan, setChan] = useState(["general"]);
 
   const API_URL = "http://localhost:4000/api/message/";
-  const config = {
-    headers: { Authorization: `Bearer ${token}` },
-    params: {
-      room: room
-    }
-  };
-  const getMessage = async (e) => {
+
+  const getMessage = async (name) => {
     await axios
-      .get(
-        API_URL,         
-        config
-      )
+      .get(API_URL, 
+        { headers: { Authorization: `Bearer ${token}` },
+        params: {
+          room: name,
+        },})
       .then((response) => {
         console.log(response.data);
-        dispatch({type:'CLEAR_MESSAGE'})
-        response.data.forEach(item=> 
-          dispatch({type:'ADD_MESSAGE', payload:{ id: item._id, text: item.text, name: item.user.name}})
-          )
+        dispatch({ type: "CLEAR_MESSAGE" });
+        response.data.forEach((item) =>
+          dispatch({
+            type: "ADD_MESSAGE",
+            payload: { id: item._id, text: item.text, name: item.user.name },
+          })
+        );
       })
       .catch((err) => {
         console.log(err.response.data.message);
       });
   };
 
-  const handleChange =(e) =>{
-    setRoom(e.target.value)
-  }
+  const handleChange = (e) => {
+    setRoom(e.target.value);
+  };
 
   const handleClick = () => {
     setOpen(!open);
@@ -71,16 +72,21 @@ function RoomList({socket}) {
     setOpen2(false);
   };
 
+  const channelClick =(name)=>{
+    getMessage(name);
+   
+  }
+
   const handleSubmit = () => {
-    dispatch({type: 'SET_ROOM', payload: {room}})
-    console.log({room})
+    dispatch({ type: "SET_ROOM", payload: { room } });
+    console.log({ room });
+    setChan([...chan, room]);
     setOpen2(false);
-    getMessage();
+    getMessage(room);
   };
 
-  
   return (
-   <>
+    <>
       <List
         sx={{
           width: "100%",
@@ -98,9 +104,9 @@ function RoomList({socket}) {
           ></ListSubheader>
         }
       >
-        <ListItemButton onClick={handleClick} >
-          <ListItemIcon sx={{minWidth:'0px', marginRight: '0.5rem'}}  >
-            <ArrowRightIcon  color="grey" />
+        <ListItemButton onClick={handleClick}>
+          <ListItemIcon sx={{ minWidth: "0px", marginRight: "0.5rem" }}>
+            <ArrowRightIcon color="grey" />
           </ListItemIcon>
           <ListItemText
             primary="Channels "
@@ -113,55 +119,62 @@ function RoomList({socket}) {
           />
           {open ? <ExpandLess /> : <ExpandMore />}
         </ListItemButton>
-        <Collapse in={open} timeout="auto" unmountOnExit >
+        <Collapse in={open} timeout="auto" unmountOnExit>
           <List component="div" disablePadding dense>
-            <ListItemButton sx={{ pl: 4 }}>
-              <ListItemIcon sx={{minWidth:'0px', marginRight: '0.5rem'}}>
-                <TagIcon color="grey" />
-              </ListItemIcon>
-              <ListItemText
-                primary="general"
-                primaryTypographyProps={{
-                  fontSize: 15,
-                  fontWeight: "medium",
-                  lineHeight: "20px",
-                  mb: "2px",
-                }}
-              />
-            </ListItemButton>
-            <ListItemButton onClick={handleClickOpen } sx={{ pl: 4 }}>
-              <ListItemIcon sx={{minWidth:'0px', marginRight: '0.5rem'}}>
-                <AddIcon color="grey" />
-              </ListItemIcon>
-              <ListItemText
-                primary="Add channels"
-                primaryTypographyProps={{
-                  fontSize: 15,
-                  fontWeight: "medium",
-                  lineHeight: "20px",
-                  mb: "2px",
-                }}
-              />
-            </ListItemButton>
+            {chan.map((cha, index) => (
+              <ListItem>
+                <ListItemButton onClick={()=>channelClick(chan[index])} sx={{ pl: 4 }}>
+                  <ListItemIcon sx={{ minWidth: "0px", marginRight: "0.5rem" }}>
+                    <TagIcon color="grey" />
+                  </ListItemIcon>
+                  <ListItemText
+                    primary={chan[index]}
+                    primaryTypographyProps={{
+                      fontSize: 15,
+                      fontWeight: "medium",
+                      lineHeight: "20px",
+                      mb: "2px",
+                    }}
+                  />
+                </ListItemButton>
+              </ListItem>
+            ))}
+
+            <ListItem>
+              <ListItemButton onClick={handleClickOpen} sx={{ pl: 4 }}>
+                <ListItemIcon sx={{ minWidth: "0px", marginRight: "0.5rem" }}>
+                  <AddIcon color="grey" />
+                </ListItemIcon>
+                <ListItemText
+                  primary="Add channels"
+                  primaryTypographyProps={{
+                    fontSize: 15,
+                    fontWeight: "medium",
+                    lineHeight: "20px",
+                    mb: "2px",
+                  }}
+                />
+              </ListItemButton>
+            </ListItem>
           </List>
         </Collapse>
         <ListItemButton onClick={handleClick1}>
-          <ListItemIcon sx={{minWidth:'0px', marginRight: '0.5rem'}}>
+          <ListItemIcon sx={{ minWidth: "0px", marginRight: "0.5rem" }}>
             <ArrowRightIcon color="grey" />
           </ListItemIcon>
           <ListItemText primary="Direct messages " />
           {open ? <ExpandLess /> : <ExpandMore />}
         </ListItemButton>
         <Collapse in={open1} timeout="auto" unmountOnExit>
-          <List component="div" disablePadding dense> 
+          <List component="div" disablePadding dense>
             <ListItemButton sx={{ pl: 4 }}>
-              <ListItemIcon sx={{minWidth:'0px', marginRight: '0.5rem'}}>
+              <ListItemIcon sx={{ minWidth: "0px", marginRight: "0.5rem" }}>
                 <TagIcon color="grey" />
               </ListItemIcon>
               <ListItemText primary="You" />
             </ListItemButton>
             <ListItemButton sx={{ pl: 4 }}>
-              <ListItemIcon sx={{minWidth:'0px', marginRight: '0.5rem'}}>
+              <ListItemIcon sx={{ minWidth: "0px", marginRight: "0.5rem" }}>
                 <AddIcon color="grey" />
               </ListItemIcon>
               <ListItemText primary="Add teammates" />
@@ -169,45 +182,39 @@ function RoomList({socket}) {
           </List>
         </Collapse>
       </List>
-      <Dialog open={open2} onClose={handleClose} maxWidth='xs' fullWidth={true}>
+      <Dialog open={open2} onClose={handleClose} maxWidth="xs" fullWidth={true}>
         <DialogTitle>Create a channel</DialogTitle>
         <DialogContent>
           <DialogContentText gutterBottom>
-          Channels are where your team communicates. 
-          They’re best when organized around a topic — #marketing, for example.
-          </DialogContentText >
-          <DialogContentText  variant="subtitle1">
-            Name
+            Channels are where your team communicates. They’re best when
+            organized around a topic — #marketing, for example.
           </DialogContentText>
+          <DialogContentText variant="subtitle1">Name</DialogContentText>
           <TextField
             autoFocus
             margin="dense"
             id="room"
             value={room}
             onChange={handleChange}
-           
             fullWidth
             variant="outlined"
           />
-           <DialogContentText variant="subtitle1" >
-           Description 
-          </DialogContentText>
+          <DialogContentText variant="subtitle1">Description</DialogContentText>
           <TextField
             autoFocus
             margin="dense"
             id="name"
-            
-            
             fullWidth
             variant="outlined"
           />
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleSubmit} variant="contained" color='success'>Create</Button>
+          <Button onClick={handleSubmit} variant="contained" color="success">
+            Create
+          </Button>
         </DialogActions>
       </Dialog>
-      </>
-    
+    </>
   );
 }
 export default RoomList;
